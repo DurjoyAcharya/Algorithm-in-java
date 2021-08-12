66cd1324c68c6a1d678c6861780c5e5582312b46
Durjoy <da-durjoy@outlook.com>
2021-08-13 00:17:29 +0600

    InsertionSort Works better now

--- src/main/java/Sorts/InsertionSort.java
+++ src/main/java/Sorts/InsertionSort.java
@@ -1,5 +1,26 @@
 package Sorts;
 
 public class InsertionSort<T extends Comparable<T>>{
-
+    private InsertionSort() {}//here private constructor directly object creation not possible use Junit with mockito
+    public static <T extends Comparable<T>> T[] sort(T[] unsorted)
+    {
+        int length= unsorted.length;
+        for (int i = 0; i < length; i++) {
+            sort(i,unsorted);
+        }
+        return unsorted;
+    }
+    private static <T extends Comparable<T>> void sort(int index,T[] unsorted)
+    {
+        for (int j = index; j>0 ; j--) {
+            T JE=unsorted[j]; //JE expressed as Jth element
+            T JME=unsorted[j-1];//JME expressed as Jth minus one element
+            if (JE.compareTo(JME)<0)
+            {
+                unsorted[j-1]=JE;
+                unsorted[j]=JME;
+            }
+            else break;
+        }
+    }
 }
